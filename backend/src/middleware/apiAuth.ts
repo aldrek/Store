aa6b93dc38d7c01f2ac123caf6071adfd6706d30
Ahmad salman <ahmadg.salman@hotmail.com>
2023-02-14 01:36:02 +0200

    feat: add api_key check middleware to protect api

--- backend/src/middleware/apiAuth.ts
+++ backend/src/middleware/apiAuth.ts
@@ -3,7 +3,7 @@ import { Request, Response, NextFunction } from "express";
 export const apiAuth = (req: Request, res: Response, next: NextFunction) => {
   const apiKey = req.headers.api_key;
   if (process.env.API_KEY !== apiKey) {
-    res.send({
+    res.status(400).send({
       message: "You are not allowed to use this api",
     });
   } else {
